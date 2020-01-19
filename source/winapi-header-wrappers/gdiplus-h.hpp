e20e1673158a58f708da46139d6764dcde784e55
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-01-19 14:32:46 +0100

    -

--- source/winapi-header-wrappers/gdiplus-h.hpp
+++ source/winapi-header-wrappers/gdiplus-h.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <header-wrappers/winapi/windows-h.hpp>
 #include <wtypes.h>         // For MinGW g++, defines PROPID type alias as ULONG.
 #include <gdiplus.h>
---
+++ source/winapi-header-wrappers/wincrypt-h.hpp
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+#include <header-wrappers/winapi/windows-h.hpp>
+#include <wincrypt.h>
