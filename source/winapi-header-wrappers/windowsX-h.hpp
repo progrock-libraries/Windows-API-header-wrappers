178b05f5e361931977431a1fc97420c803117bc7
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-06-09 15:02:30 +0200

    Update windowsX-h.hpp

--- source/winapi-header-wrappers/windowsX-h.hpp
+++ source/winapi-header-wrappers/windowsX-h.hpp
@@ -1,3 +1,6 @@
 #pragma once
 #include <winapi-header-wrappers/windows-h.hpp>
 #include <windowsx.h>
+
+// HANDLE_WM_NOTIFY is defined by <commctrl.h>.
+// auto on_wm_notify( const HWND, const int control_id, const Type_<const NMHDR*> p_params ) -> LRESULT
