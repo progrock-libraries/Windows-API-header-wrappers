ad02b7e8145b41311b3d5b42c297eab409a6feea
alf-p-steinbach <alf.p.steinbach@gmail.com>
2021-06-23 17:03:08 +0200

    Update windows-h.hpp

--- source/library/windows-api-header-wrappers/windows-h.hpp
+++ source/library/windows-api-header-wrappers/windows-h.hpp
@@ -1,19 +1,15 @@
-#pragma once
+﻿#pragma once
+// Ideal usage:
+// define `WINAPI_VERSION` in the build settings, or anyway before including this file.
 
 #ifdef MessageBox
 #   error "<windows.h> has already been included, possibly with undesired options."
 #endif
 
 #include <assert.h>
-#ifdef _MSC_VER
+#ifdef _MSC_VER         // TODO: Check version.
 #   include <iso646.h>                  // Standard `and` etc. also with MSVC.
 #endif
-
-#ifndef _WIN32_WINNT
-#   define _WIN32_WINNT     0x0600      // Windows Vista as earliest supported OS.
-#endif
-#undef WINVER
-#define WINVER _WIN32_WINNT
 
 // UTF8_WINAPI is a custom macro for this file. UNICODE, _UNICODE and _MBCS are MS macros.
 #if defined( UTF8_WINAPI ) and defined( UNICODE )
@@ -37,36 +33,81 @@
 #   define UNICODE
 #   define _UNICODE     // Mainly for 3rd party code that uses it for platform detection.
 #endif
-#undef NOMINMAX
-#define NOMINMAX
-#undef STRICT
+#undef STRICT           // C++-compatible “strongly typed” declarations, please.
 #define STRICT
+#undef NOMINMAX         // No C++-sabotaging “min” and “max” macros, please.
+#define NOMINMAX
+
+// Reduce the size of the <windows.h>, to the degree practically possible.
+// Also, with WIN32_LEAN_AND_MEAN an `#include <winsock2.h>` will actually include that header.
 #undef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
-// After this an `#include <winsock2.h>` will actually include that header.
+#undef NOCOMM           // No serial comms API please.
+#define NOCOMM
+#define NOMCX           // No modem configuration API please.
+#define NOMCX
+#undef NOOPENFILE       // No OpenFile function please; it's limited and long deprecated.
+#define NOOPENFILE
+
+// With much stuff introduced in Windows Vista and later one needs to set version indicators.
+// NTDDI_VERSION (newest macro), _WIN32_WINNT and WINVER (oldest macro) are used by <windows.h>.
+// WINAPI_VERSION and WINAPI_SERVICE_PACK are macros used by and optionally defined by this header.
+//
+// Experience shows that one needs to take full control of this, especially with g++, so:
+//
+#if defined( WINAPI_VERSION )
+#   undef   NTDDI_VERSION
+#   undef   _WIN32_WINNT
+#   undef   WINVER
+#else
+#   undef WINAPI_SERVICE_PACK
+#   if defined( NTDDI_VERSION )
+#       undef   _WIN32_WINNT
+#       undef   WINVER
+#       define  WINAPI_VERSION          (NTDDI_VERSION >> 16)
+#       define  WINAPI_SERVICE_PACK     (NTTDI_VERSION & 0xFFFF)
+#   elif defined( _WIN32_WINNT )
+#       undef   WINVER
+#       define  WINAPI_VERSION          _WIN32_WINNT
+#   elif defined( WINVER )
+#       define  WINAPI_VERSION          WINVER
+#   else
+#       define  WINAPI_VERSION          0x0600      // Default, Windows Vista.
+#   endif
+#endif
+
+#ifndef WINAPI_SERVICE_PACK
+#   define  WINAPI_SERVICE_PACK         0
+#endif
+
+#ifndef NTDDI_VERSION
+#   define NTDDI_VERSION    ((WINAPI_VERSION << 16) | WINAPI_SERVICE_PACK)
+#endif
+
+#ifndef _WIN32_WINNT
+#   define _WIN32_WINNT     WINAPI_VERSION
+#endif
+
+#ifndef WINVER
+#   define WINVER           WINAPI_VERSION
+#endif
 
 
 /////////////////////////////////////////////////////////////////////
 #include <windows.h>                                                //
 /////////////////////////////////////////////////////////////////////
 
-
+// Use like `static_assert( IS_NARROW_WINAPI() )`.
 #define IS_NARROW_WINAPI() \
     ("Define UTF8_WINAPI please.", sizeof(*GetCommandLine()) == 1)
 
+// Use like `static_assert( IS_WIDE_WINAPI() )`.
 #define IS_WIDE_WINAPI() \
     ("Define UNICODE please.", sizeof(*GetCommandLine()) > 1)
 
 inline auto winapi_h_assert_utf8_codepage()
     -> bool
 {
-    #ifdef __GNUC__
-        #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wunused-value"
-    #endif
-    assert(( "The process codepage isn't UTF-8 (old Windows?).", GetACP() == 65001 ));
-    #ifdef __GNUC__
-        #pragma GCC diagnostic pop
-    #endif
+    assert( "The process codepage isn't UTF-8 (old Windows?)." && GetACP() == 65001 );
     return true;
 }
